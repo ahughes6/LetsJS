da1c09f1ed3b7209776b4d1f1c864c1851791991
Nils Wisiol <info@nils-wisiol.de>
2014-04-06 03:17:23 -0400

    feat(game): show score

--- public/controller/home.js
+++ public/controller/home.js
@@ -25,19 +25,24 @@ angular.module('letsjs.controllers').controller('HomeController', function($scop
   $scope.setupPlayer = function(nick) {
     socket.emit('join', { nick: nick });
     $scope.state=1;
+    tStart = new Date().getTime();
+    updateScoreInterval = $interval(updateScore, 100);
   }
 
   $scope.leaveGame = function() {
     socket.emit('leave');
     $scope.state=0;
+    $interval.cancel(updateScoreInterval);
   }
 
   var t0 = new Date().getTime();
+  var tStart = new Date().getTime();
 
   var myInterval = $interval(extrapolate, 10);
 
   $scope.$on("$destroy", function(){
     $interval.cancel(myInterval);
+    $interval.cancel(updateScoreInterval);
 
     if($scope.state == 1) {
       socket.emit('leave');
@@ -45,6 +50,8 @@ angular.module('letsjs.controllers').controller('HomeController', function($scop
   });
 
   function extrapolate() {
+    if ($scope.offline)
+      return; // don't extrapolate during offline time
     var current = new Date().getTime(); // get current time
     var t = (current - t0) / 1000.0; // calculate time passed since last execution (in seconds)
     t0 = current; // save current time for next execution
@@ -52,6 +59,11 @@ angular.module('letsjs.controllers').controller('HomeController', function($scop
       newton(t,$scope.objects[id]);
     });
   };
+  
+  var updateScoreInterval;
+  function updateScore() {
+    $scope.score = Math.round((new Date().getTime() - tStart) / 1000, 1);
+  }
 
   function newton(t, obj) {
     var coords = ['x', 'y'];
