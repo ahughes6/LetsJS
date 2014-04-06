ce594086bb5ea9e6cbf8100d8cb06f4b5deb5271
Nils Wisiol <info@nils-wisiol.de>
2014-04-05 21:59:36 -0400

    feat(players): objects are added corresponding to the players

--- player.js
+++ player.js
@@ -1,20 +1,38 @@
 var playerlist = [];
+var gameloop = require('./gameloop');
 
 function add(socket, nick) {
   playerlist.push({
     socket: socket,
     nick: nick,
   });
+  gameloop.objects.push(
+    {
+      p: {x: 0, y: 0},
+      v: {x: 0, y:10},
+      a: {x: 0, y: 0},
+      player: nick,
+    }
+  );
 }
 
 function remove(socket) {
   var toBeRemoved = null;
+  var nick = null;
   playerlist.forEach(function(p) {
     if (p.socket===socket)
       toBeRemoved = p;
   });
-  if (toBeRemoved)
+  if (toBeRemoved) {
     playerlist.splice(playerlist.indexOf(toBeRemoved), 1);
+    gameloop.objects.forEach(function(o) {
+      if (o.nick==nick) {
+        toBeRemoved = o;
+      }
+    });
+    if (toBeRemoved)
+      gameloop.objects.splice(toBeRemoved, 1);
+  }
 }
 
 function die(player) {
